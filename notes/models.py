3095ed423b49c78c668daab0decef272e223e5c3
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2025-06-06 21:12:29 +0300

    Add notes app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	note_text = models.CharField(max_length=500)
+	important = models.BooleanField(default=False)
+	pub_date = models.DateTimeField("date published")
+
+	def __str__(self):
+		return self.note_text
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from notes.models import Note
 
 def index(request):
-	return HttpResponse('Hello world, this is the notes index. More coming soon')
+    latest_note_list = Note.objects.order_by("-pub_date")[:5]
+    output = ", ".join([n.note_text for n in latest_note_list])
+    return HttpResponse(output)
+
+
+def detail(request, note_id):
+    return HttpResponse("You're looking at note %s." % note_id)

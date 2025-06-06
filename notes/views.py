e85a06f4fc17c303ef65c3b1cdebb7f0ef4677f4
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2025-06-06 21:18:18 +0300

    Update views

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from notes.models import Note
 
 def index(request):
     latest_note_list = Note.objects.order_by("-pub_date")[:5]
-    output = ", ".join([n.note_text for n in latest_note_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_note_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, note_id):
-    return HttpResponse("You're looking at note %s." % note_id)
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, "polls/detail.html", {"question": note})

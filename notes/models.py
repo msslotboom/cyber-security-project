cf76620ae6adce6a9429c462f6f4429b06403348
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2025-06-06 22:26:48 +0300

    Remove redundant field from note model

--- notes/models.py
+++ notes/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class Note(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
 	note_text = models.CharField(max_length=500)
-	important = models.BooleanField(default=False)
 	pub_date = models.DateTimeField("date published")
 
 	def __str__(self):
